3c9aa0650bf0e047d52bdcfe940ea147ad8d2472
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-02-08 15:55:06 +0530

    Create factorial and ncr function.cpp

---
+++ factorial and ncr function.cpp
@@ -0,0 +1,28 @@
+//call stack
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    
+    int ans = 1;
+    for(int i=1;i<=n;i++){
+        ans = ans*i;
+    }
+    return ans;
+}
+
+int NCR(int n,int r){
+    
+    int ans = factorial(n)/(factorial(n-r)*factorial(r));
+    return ans;
+    
+}
+
+int main() {
+    //NCR, and Factorial of Number
+    cout<<factorial(5)<<endl;
+    cout<<NCR(5,2)<<endl;
+    
+    
+    return 0;
+}
