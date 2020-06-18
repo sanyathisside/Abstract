2577440a49a5a8e1c3ba6b413c592d81efc182c5
Sanya Kumari <1701sanya@gmail.com>
2020-06-18 12:40:04 +0530

    Create lcm.cpp

---
+++ Number Theory/lcm.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+int gcd(int a, int b) 
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b);  
+      
+} 
+
+int lcm(int a, int b)
+{
+	return a*b/gcd(a,b);
+}
+   int main() 
+{ 
+int c,d;
+cin>>c>>d;
+    cout<<lcm(c,d);
+	return 0; 
+} 
