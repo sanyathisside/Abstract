1ed02e01799b540b336ee9a6ca697b518878675d
Sanya Kumari <1701sanya@gmail.com>
2020-06-18 12:37:51 +0530

    Create gcd.cpp

---
+++ Number Theory/gcd.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+int gcd(int a, int b) 
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b);  
+      
+} 
+   int main() 
+{ 
+int c,d;
+cin>>c>>d;
+    cout<<gcd(c,d);
+	return 0; 
+} 
